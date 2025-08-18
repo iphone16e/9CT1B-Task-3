8257df38406d89881a93f20475ce77685f84e5f3
Minh Mai <minh.mai4@DETNSW>
2025-08-18 10:35:17 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Load the dataset
+df = pd.read_csv("ResearchInformation3(1).csv")
+
+# Show the first 5 rows
+print(df.head())
+
+# Show the column names
+print(df.columns)
